8e7716fe77a1b2305a6c031595a5e80bae855613
Tyler Newman <tylerjnewman18@gmail.com>
2020-12-09 20:28:22 -0800

    Create example home page

--- src/apollo/graphql/resolvers/User/index.ts
+++ src/apollo/graphql/resolvers/User/index.ts
@@ -1,9 +1,102 @@
 import {IResolvers} from 'apollo-server-micro'
+import {Database, User} from 'apollo/lib/types'
+// import {authorize} from 'apollo/lib/utils'
+import {NextApiRequest} from 'next'
+import {UserArgs, UserBookingsArgs, UserBookingsData} from './types'
+
+async function authorize(rdb: Database, req: NextApiRequest) {
+  console.log('hello')
+}
 
 export const userResolvers: IResolvers = {
   Query: {
-    user: () => {
-      return 'Query.user'
+    user: async (
+      _root: undefined,
+      {id}: UserArgs,
+      {db, req}: {db: Database; req: NextApiRequest},
+    ): Promise<User> => {
+      try {
+        const user = await db.users.findOne({_id: id})
+        if (!user) {
+          throw new Error("user can't be found")
+        }
+        console.log('here')
+        const viewer = await authorize(db, req)
+        console.log('not here')
+        if (viewer && viewer._id === user._id) {
+          user.authorized = true
+        }
+        return user
+      } catch (error) {
+        throw new Error(`Failed to query user: ${error}`)
+      }
+    },
+  },
+  User: {
+    id: (user: User): string => {
+      return user._id
+    },
+    hasWallet: (user: User): boolean => {
+      return Boolean(user.walletId)
+    },
+    income: (user: User): number | null => {
+      return user.authorized ? user.income : null
+    },
+    bookings: async (
+      user: User,
+      {limit, page}: UserBookingsArgs,
+      {db}: {db: Database},
+    ): Promise<UserBookingsData | null> => {
+      try {
+        if (!user.authorized) {
+          return null
+        }
+
+        const data: UserBookingsData = {
+          total: 0,
+          result: [],
+        }
+
+        let cursor = await db.bookings.find({
+          _id: {$in: user.bookings},
+        })
+
+        cursor = cursor.skip(page > 0 ? (page - 1) * limit : 0)
+        cursor = cursor.limit(limit)
+
+        data.total = await cursor.count()
+        data.result = await cursor.toArray()
+
+        return data
+      } catch (error) {
+        throw new Error(`Failed to query user bookings: ${error}`)
+      }
+    },
+    listings: async (
+      user: User,
+      {limit, page}: UserListingsArgs,
+      {db}: {db: Database},
+    ): Promise<UserListingsData | null> => {
+      try {
+        const data: UserListingsData = {
+          total: 0,
+          result: [],
+        }
+
+        let cursor = await db.listings.find({
+          _id: {$in: user.listings},
+        })
+
+        cursor = cursor.skip(page > 0 ? (page - 1) * limit : 0)
+        cursor = cursor.limit(limit)
+
+        data.total = await cursor.count()
+        data.result = await cursor.toArray()
+
+        return data
+      } catch (error) {
+        throw new Error(`Failed to query user listings: ${error}`)
+      }
     },
   },
 }
--- src/apollo/lib/utils/index.ts
+++ src/apollo/lib/utils/index.ts
@@ -5,6 +5,8 @@ export const authorize = async (
   db: Database,
   req: NextApiRequest,
 ): Promise<User | null> => {
+  console.log('entered Authorize function')
+  console.log({req})
   const token = req.headers.authorization
   const viewer = await db.users.findOne({
     _id: req.cookies.view,
